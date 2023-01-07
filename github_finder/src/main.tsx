3967794c00b0bb6a6c7bcbcf982b8b3049ee611d
Caio Melo <101616140+Greedddddd@users.noreply.github.com>
2023-01-07 22:07:30 +0000

    terceiro commit

--- github_finder/src/main.tsx
+++ github_finder/src/main.tsx
@@ -3,8 +3,26 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
+import { createBrowserRouter, RouterProvider} from "react-router-dom"
+
+//pages
+import Home from "./routes/Home";
+
+const router = createBrowserRouter([
+  {
+    path:"/",
+    element: <App />,
+    childen:[
+      {
+        path: "/",
+        element: <Home />,
+      },
+    ],
+  },
+]);
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>,
 )
