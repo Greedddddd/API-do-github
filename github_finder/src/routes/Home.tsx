eb1bb5eb90e52752600a28e00d6cb315209cd820
Caio Melo <caiomelok@gmail.com>
2023-01-13 17:56:47 -0300

    correção de bug

--- github_finder/src/routes/Home.tsx
+++ github_finder/src/routes/Home.tsx
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 import Search from "../components/Search"
-=======
-import Search from "../components/search";
->>>>>>> fe01d13378fdfe1f7c658051fb9257f8098be5b4
 
 const Home = () => {
     return <div><Search /></div>
