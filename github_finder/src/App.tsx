9e95bae3530bb7478dd5cf141f81622af0593315
Caio Melo <caiomelok@gmail.com>
2023-01-14 19:47:03 -0300

    correção

--- github_finder/src/App.tsx
+++ github_finder/src/App.tsx
@@ -1,10 +1,10 @@
 import {Outlet} from "react-router-dom";
 
-import classes from "./app.module.css";
+import classes from "./App.module.css";
 
 function App() {
   return (
-    <div className={classes.App}>
+    <div className={classes.app}>
       <h1>Github Finder</h1>
       <Outlet />
     </div>
